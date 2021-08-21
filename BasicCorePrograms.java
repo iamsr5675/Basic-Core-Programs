706582ed1d20ce2c7c36f8e563422f1fea4a973d
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-21 15:03:39 +0530

    Compute Quotient and Remainder

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -4,33 +4,18 @@ import java.util.Scanner;
 
 public class BasicCorePrograms 
 {
-	public static boolean isPrime(int n) {
-		for (int i = 2; i <= n / 2; i++) {
-			if (n % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	static void primeFactors(int n) {
-		for (int i = 2; i <= n; i++) {
-			int t = n % i;
-			if (t == 0) {
-				if (isPrime(i))
-					System.out.println(i + " ");
-			}
-		}
-	}
-
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number to find the Prime Factors:");
-		int n = sc.nextInt();
-		System.out.println("Prime Factors are:");
-		primeFactors(n);
-		// System.out.println(isPrime(n));
-		sc.close();
+		int dividend, divisor, quotient, remainder;
+		System.out.println("Enter the value of Dividend:");
+		dividend = sc.nextInt();
+		System.out.println("Enter the value of Divisor:");
+		divisor = sc.nextInt();
+		quotient = dividend / divisor;
+		remainder = dividend % divisor;
+		System.out.println("Value of Quotient : " +quotient);
+		System.out.println("Value of Remainder : " +remainder);
 
 	}
 }
