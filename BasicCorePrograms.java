a04eae0506063573a151180531e1e32bd685ba46
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-21 14:43:19 +0530

    Prime Factorization

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -4,21 +4,33 @@ import java.util.Scanner;
 
 public class BasicCorePrograms 
 {
-	public static void main(String[] args) {
-
-		double result = 0, num = 0, j;
-		int n;
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter the Harmonic Value: ");
-		n = sc.nextInt();
-
-		for (j = 1; j <= n; j++) {
-			num = num + (1 / j);
-			result = num;
+	public static boolean isPrime(int n) {
+		for (int i = 2; i <= n / 2; i++) {
+			if (n % i == 0) {
+				return false;
+			}
 		}
-		System.out.println("the value of harmonic no. " + n + " is: " + result);
-
+		return true;
 	}
 
+	static void primeFactors(int n) {
+		for (int i = 2; i <= n; i++) {
+			int t = n % i;
+			if (t == 0) {
+				if (isPrime(i))
+					System.out.println(i + " ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to find the Prime Factors:");
+		int n = sc.nextInt();
+		System.out.println("Prime Factors are:");
+		primeFactors(n);
+		// System.out.println(isPrime(n));
+		sc.close();
+
+	}
 }
