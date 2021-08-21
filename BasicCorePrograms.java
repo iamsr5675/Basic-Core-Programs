58bdfc7591c14c534dbe979146358ec62b0ee7e4
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-21 13:13:15 +0530

    Leap Year

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -4,27 +4,26 @@ import java.util.Scanner;
 
 public class BasicCorePrograms 
 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int i, n, heads=0, tails=0;
-		System.out.println("Enter the number of times to flip the coin:");
-		n = sc.nextInt();
-		for(i=1; i<=n; i++)
-		{
-		if (Math.random() < 0.5)
-		{
-		System.out.println("Tails");
-		tails++;
-		}
-		else
-		{
-		System.out.println("Heads");
-		heads++;
-		}
-		}
-		double heads_percentage = ((double)heads/n)*100;
-		double tails_percentage = ((double)tails/n)*100;
-		System.out.println("Percentage of Head : " +heads_percentage);
-		System.out.println("Percentage of Tails : " +tails_percentage);
-	}
+	public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year");
+        
+            int year = sc.nextInt(); 
+            if(year>999 && year<10000)
+            {
+                if(((year%4)==0 && (year%100)!=0)||(year%400)==0)
+                {
+                    System.out.println("Leap Year");
+                }
+                else
+                {
+                    System.out.println("Not a Leap Year");
+                }
+            }
+            else
+            {
+                System.out.println("Invalid Year");
+            }        
+    }
 }
