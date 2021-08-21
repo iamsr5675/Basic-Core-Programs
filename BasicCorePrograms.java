9c15443cec8abbed10d497982b0aaded99207e4a
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-21 15:14:18 +0530

    Swap two numbers

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -6,16 +6,16 @@ public class BasicCorePrograms
 {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int dividend, divisor, quotient, remainder;
-		System.out.println("Enter the value of Dividend:");
-		dividend = sc.nextInt();
-		System.out.println("Enter the value of Divisor:");
-		divisor = sc.nextInt();
-		quotient = dividend / divisor;
-		remainder = dividend % divisor;
-		System.out.println("Value of Quotient : " +quotient);
-		System.out.println("Value of Remainder : " +remainder);
-
-	}
-}
+		int x, y, t; 
+	       Scanner sc = new Scanner(System.in);  
+	       System.out.println("Enter the value of X and Y");  
+	       x = sc.nextInt();  
+	       y = sc.nextInt();  
+	       System.out.println("Before Swapping Numbers: "+x +"  "+ y);    
+	       t = x;  
+	       x = y;  
+	       y = t;  
+	       System.out.println("After Swapping: "+x +"   " + y);  
+	       System.out.println( );  
+	    }    
+	}  
