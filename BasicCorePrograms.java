43ca46112037c03e80ec5730cb3c06f6af944783
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-21 16:23:58 +0530

    Largest Among Three Numbers

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class BasicCorePrograms 
 {
 	
-	public static void main(String args[]){
-	      System.out.println("Enter a character :");
-	      Scanner sc = new Scanner(System.in);
-	      char ch = sc.next().charAt(0);
-	      if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == 'A'|| ch == 'E'|| ch == 'I' ||ch == 'O' ||ch == 'U')
-	      {
-	         System.out.println("Given character is an Vowel");
-	      }
-	      else
-	      {
-	         System.out.println("Given character is a Consonant");
-	      }
-	   }
-	}
+	public static void main(String[] args)   
+	{  
+	int a, b, c, largest, temp;  
+	Scanner sc = new Scanner(System.in);  
+	System.out.println("Enter the first number:");  
+	a = sc.nextInt();  
+	System.out.println("Enter the second number:");  
+	b = sc.nextInt();  
+	System.out.println("Enter the third number:");  
+	c = sc.nextInt();    
+	temp=a>b?a:b;    
+	largest=c>temp?c:temp;    
+	System.out.println("The largest number is: "+largest);  
+	}  
+	}  
