fb75b5e64d8243812656e35fe12a9cb5eafb3c77
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-21 16:18:57 +0530

    Check Whether an Alphabet is Vowel or Consonant

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class BasicCorePrograms 
 {
 	
-	public static void main(String[] args) {
-		int n; 
-	       Scanner sc = new Scanner(System.in);  
-	       System.out.println("Enter the Number:");  
-	       n=sc.nextInt();
-	       if (n % 2 == 0)
-	       {
-	    	   System.out.println("Even Number");
-	       }
-	       else
-	       {
-	    	   System.out.println("Odd Number");
-	       }
-	    }    
-	}  
+	public static void main(String args[]){
+	      System.out.println("Enter a character :");
+	      Scanner sc = new Scanner(System.in);
+	      char ch = sc.next().charAt(0);
+	      if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == 'A'|| ch == 'E'|| ch == 'I' ||ch == 'O' ||ch == 'U')
+	      {
+	         System.out.println("Given character is an Vowel");
+	      }
+	      else
+	      {
+	         System.out.println("Given character is a Consonant");
+	      }
+	   }
+	}
